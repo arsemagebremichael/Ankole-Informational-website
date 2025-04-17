eb88a05c3f556780e7f28efc504448379543c978
A <Arsema.arsemaagebremichael@gmail.com>
2025-04-17 20:47:56 +0300

    adding review

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-let slideIndex = 1;
+var slideIndex = 1;
 showDivs(slideIndex);
-
 function plusDivs(n) {
-  showDivs(slideIndex += n);
+    showDivs(slideIndex += n);
 }
-
 function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";  
+    var i;
+    var slides = document.getElementsByClassName("review-Slides");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
 }
